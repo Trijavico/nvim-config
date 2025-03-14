9e1d6eb76543190e2eea8167c93ef2bb687c0397
Trijavico <javier.penaperez08@gmail.com>
2025-03-13 21:28:43 -0400

    change keymap for floating terminal

--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -1,5 +1,5 @@
 local fterm = require("FTerm")
 
-vim.keymap.set({ "n", "t" }, "<leader>,", function() 
-	fterm.toggle() 
+vim.keymap.set({ "n", "t" }, "<A-,>", function()
+	fterm.toggle()
 end, { noremap = true, silent = true })
